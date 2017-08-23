76950bf91cf1cf896e66eb4c3036376569176024
LAPTOP-P78HKCID\Srikanth <Sriramdassrikanth7@gmail.com>
2017-08-22 20:03:36 -0700

    new commit

---
+++ src/arraysdemo.java
@@ -0,0 +1,18 @@
+
+public class arraysdemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+     
+		//int arr[]= {10,20,30,40,50};
+	int []arr= new int[5];
+	arr[0]=10;
+	arr[1]=20;
+		System.out.println("arr is" +arr);
+	
+	for(int i=0;i<5;i++)
+		
+		System.out.println(arr[i]);
+	}
+
+}
